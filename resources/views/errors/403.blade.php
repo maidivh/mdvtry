27ed83b1e4b8a8b1c4ef0986453384943cfadaaf
vagrant <vagrant@homestead>
2017-05-27 02:02:28 +0000

    finish static pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>403错误</title>
+
+    <script src="/js/jquery.js"></script>
+    <link rel="stylesheet" href="/css/app.css">
+    <style>
+        body {
+            padding-top: 0px;
+        }
+        .error404{
+            width: 1050px;
+            margin: 30px auto;
+        }
+        .Sorry {
+            font-size: 16px;
+            font-weight: 700;
+            margin-bottom: -17px;
+            display: inline-block;
+        }
+        .wearesorry{
+            color: #b1b1b1;
+            margin-bottom: -5px;
+            font-size: 13px;
+            margin-left: 86px;
+        }
+        .error404-content{
+            font-size:14px;
+            color: #333333;
+        }
+        .imgasee{
+            width: 80px;
+            height: 125px;
+            display: inline-block;
+            margin-bottom: -38px;
+            background: url(/statics/image/5269dfb080827.png);
+        }
+        ul{
+            margin-top: 8px;
+        }
+        li{
+            line-height: 25px;
+        }
+    </style>
+</head>
+<body>
+
+<div class="error404">
+    <span class="imgasee"></span>
+    <p class="Sorry">抱歉，您的权限不足,无法请求……</p>
+    <p class=wearesorry>We're sorry but the page your are looking for is Not Found...</p>
+    <hr>
+    <div class=error404-content>
+        仔细找过啦，没有发现你要找的页面。最可能的原因是：
+        <ul>
+            <li>在地址中可能存在键入错误。
+            <li>当你点击某个链接时，它可能已过期。</li>
+        </ul>
+        <strong>点击以下链接继续浏览力图数字科技网站上的其它内容</strong>（<a href="http://www.designfrontline.com/" target=_blank>http://www.designfrontline.com/</a>）：
+        <div class=show14>
+            <ul>
+                <li><a title=返回首页 href="/">返回首页</a>
+                <li><a title=Github地址 href="https://github.com/maidivh">Github地址</a>
+                <li><a title=Laraver社区 href="https://laravel-china.org/">Laraver社区</a>
+                <li><a title=返回上一个页面 href="javascript:history.back(-1)">返回上一页</a>
+                </li>
+            </ul>
+        </div>
+        　
+    </div>
+
+
+</div>
+
+
+</body>
+</html>
+
+
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404错误</title>
+
+    <script src="/js/jquery.js"></script>
+    <link rel="stylesheet" href="/css/app.css">
+    <style>
+        body {
+            padding-top: 0px;
+        }
+        .error404{
+            width: 1050px;
+            margin: 30px auto;
+        }
+        .Sorry {
+            font-size: 16px;
+            font-weight: 700;
+            margin-bottom: -17px;
+            display: inline-block;
+        }
+        .wearesorry{
+            color: #b1b1b1;
+            margin-bottom: -5px;
+            font-size: 13px;
+            margin-left: 86px;
+        }
+        .error404-content{
+            font-size:14px;
+            color: #333333;
+        }
+        .imgasee{
+            width: 80px;
+            height: 125px;
+            display: inline-block;
+            margin-bottom: -38px;
+            background: url(/statics/image/5269dfb080827.png);
+        }
+        ul{
+            margin-top: 8px;
+        }
+        li{
+            line-height: 25px;
+        }
+    </style>
+</head>
+<body>
+
+<div class="error404">
+    <span class="imgasee"></span>
+    <p class="Sorry">抱歉，找不到您要的页面……</p>
+    <p class=wearesorry>We're sorry but the page your are looking for is Not Found...</p>
+    <hr>
+    <div class=error404-content>
+        仔细找过啦，没有发现你要找的页面。最可能的原因是：
+        <ul>
+            <li>在地址中可能存在键入错误。
+            <li>当你点击某个链接时，它可能已过期。</li>
+        </ul>
+        <strong>点击以下链接继续浏览Medivh之道相关的内容</strong>（<a href="http://www.baidu.com/" target=_blank>我是你能随便乱点的嘛,有种你敢点下试试！！</a>）
+        <div class=show14>
+            <ul>
+                <li><a title=返回首页 href="/">返回首页</a>
+                <li><a title=Github地址 href="https://github.com/maidivh">Github地址</a>
+                <li><a title=Laraver社区 href="https://laravel-china.org/">Laraver社区</a>
+                <li><a title=返回上一个页面 href="javascript:history.back(-1)">返回上一页</a>
+                </li>
+            </ul>
+        </div>
+        　
+    </div>
+
+    {{-- 导入底部 --}}
+
+</div>
+
+
+</body>
+</html>
+
+
